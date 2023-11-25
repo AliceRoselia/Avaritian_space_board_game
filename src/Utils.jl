d9da18ea40fe5877493a87cc40f3155ddd1e5439
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2023-11-25 18:15:59 +0700

    Screw "Moddability!"
    The minimum viable product will be hard-coded, then I will handle mod requests manually until I could find a way to generate code.

--- src/Utils.jl
+++ src/Utils.jl
@@ -23,13 +23,13 @@ struct WeaponFeature
     #Working in progress.
 end
 =#
-
+#=
 struct Feature
     numbercontent::Dict{String,Float64}
     subfeatures::Dict{String,Feature}
     Feature() = new(Dict{String,Float64}(),Dict{String,Feature}())
 end
-
+=#
 
 struct Weapon
     #These are normally integers but can be modified so float they are.
@@ -38,7 +38,7 @@ struct Weapon
     damage::Float64
     armor_penetration::Float64
     name::String
-    feature::Feature
+    #feature::Feature
 end
 
 mutable struct Unit
@@ -54,27 +54,29 @@ mutable struct Unit
     weapons::Vector{Weapon}
     name::String
     side::Int
-    selfaction::Vector{workInProgress} #Actions done to self such as healing or changing weapons.
-    actionOn::Vector{workInProgress} #Actions done to a tile.
+    #selfaction::Vector{workInProgress} #Actions done to self such as healing or changing weapons.
+    #actionOn::Vector{workInProgress} #Actions done to a tile.
 end
 
-
+#=
 struct costMovingTo
     movementcost::Float64 #The cost of movement.
     logicalcost::Float64 
     moveUnit::workInProgress #A function that units will pay the cost.
 end
-
+=#
 mutable struct Tile
 
     features::Feature
     movement_cost::Int
     height::Int
     unit::Union{Unit,Nothing}
-    adjacenttile::Vector{Tuple{Tile,costMovingTo}}
+    #adjacenttile::Vector{Tuple{Tile,costMovingTo}}
 end
 
-
+function get_adjacent_tile(tile::Tile)
+    error("Working in progress.")
+end
 
 function calculate_damage(weapon::Weapon, unit::Unit)
     #Calculation of feature working in progress.
